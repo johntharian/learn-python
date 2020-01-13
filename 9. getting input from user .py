40c054ca45a0f70da070970dd18d5eb4ddedce29
johntharian <55285376+johntharian@users.noreply.github.com>
2020-01-13 21:09:27 +0530

    Add files via upload

---
+++ 9. getting input from user .py
@@ -0,0 +1,17 @@
+# for getting input from the user use the input command  input()
+# whatever value we take from the user should be stored in a variable
+# let us ask the user for his/her name and age and lets print it
+
+
+# the variable name stores the input from the user
+name=input('Enter your name : ')
+
+# the variable age stores the age of the  user
+# since age is a number , we convert the input from the user into int type 
+age=int(input('Enter your age : '))
+
+# printing the output
+print(f" My name  is {name} and I'm {age} years old . ")
+
+# end
+
