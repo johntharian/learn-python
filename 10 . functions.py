67f656817d24b71759ddd1b6a022d399c798461f
johntharian <55285376+johntharian@users.noreply.github.com>
2020-01-13 21:22:23 +0530

    Add files via upload

---
+++ 10 . functions.py
@@ -0,0 +1,23 @@
+'''
+for writing funtions in python we follow the given syntax
+
+def function_name():
+    declaring variables
+    assigning values,etc
+
+In python a function is defined using the def command
+
+we should call the function at the end of defining it 
+
+The syntax for calling is like this
+
+function_name()
+
+'''
+# Writing function
+
+def name():
+    print("My  first python function")
+
+#calling the function
+name()
