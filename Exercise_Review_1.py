9c245a747a1017befdc77a5b30c1c1766b987b75
johntharian <jtharian13@gmail.com>
2020-02-16 22:18:05 +0530

    excercise 1

--- Exercise_Review_1.py
+++ Exercise_Review_1.py
@@ -11,20 +11,20 @@
 
 # Given the string:
 s = 'django'
-
+print(f"s = django ")
 # Use indexing to print out the following:
 # 'd'
-
+print(s[0])
 # 'o'
-
+print(s[-1])
 # 'djan'
-
+print(s[0:4])
 # 'jan'
-
+print(s[1:4])
 # 'go'
-
+print(s[4:])
 # Bonus: Use indexing to reverse the string
-
+print(s[::-1])
 
 ###############
 ## Problem 2 ##
@@ -32,8 +32,10 @@ s = 'django'
 
 # Given this nested list:
 l = [3,7,[1,4,'hello']]
+print("l = [3,7,[1,4,'hello']]")
 # Reassign "hello" to be "goodbye"
-
+l[2][2]="goodbye"
+print(l)
 
 ###############
 ## Problem 3 ##
@@ -43,9 +45,16 @@ l = [3,7,[1,4,'hello']]
 
 d1 = {'simple_key':'hello'}
 
+print(d1['simple_key'])
+
+
 d2 = {'k1':{'k2':'hello'}}
 
+print(d2['k1']['k2'])
+
 d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+
+print(d3['k1'][0]['nest_key'][1])
 
 
 ###############
@@ -54,7 +63,7 @@ d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
 
 # Use a set to find the unique values of the list below:
 mylist = [1,1,1,1,1,2,2,2,2,3,3,3,3]
-
+print(set(mylist))
 
 ###############
 ## Problem 5 ##
@@ -66,3 +75,5 @@ name = "Sammy"
 
 # Use print formatting to print the following string:
 "Hello my dog's name is Sammy and he is 4 years old"
+
+print(f"Hello my dog's name is {name} and he is {age} years old")
